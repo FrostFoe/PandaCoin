3a6d29873adb7174b1668d9491300a00892c3deb
FrostFoe <frostfoe@gmail.com>
2025-06-22 03:35:02 +0000

    {"error":"Error: State not found.\nTo resolve this error please visit ou

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,6 +1,8 @@
 import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
 import { type NextRequest } from "next/server";
 
+export const dynamic = "force-dynamic";
+
 export async function GET(
   request: NextRequest,
   { params }: { params: { kindeAuth: string } },
